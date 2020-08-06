bed174742ee3e423591e995c081bdaf569d87399
Polina Nguen <polina.nguen@optimizely.com>
2020-08-05 18:42:16 -0700

    Improve updateDiagnostics

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import { OptimizelyService } from './optimizelyService';
 import { OP_MODE_JS, OP_MODE_TS } from './providers';
 
+const REGEX = /.*\.(getFeatureVariable|getFeatureVariableDouble|getFeatureVariableInteger|getFeatureVariableString|getFeatureVariableBoolean|getFeatureVariableJSON|activate|getAllFeatureVariables|isFeatureEnabled)\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g;
+
 let activeEditor = vscode.window.activeTextEditor;
 let diagnosticCollection: vscode.DiagnosticCollection;
 
@@ -11,70 +13,58 @@ export function activateDiagnostics(ctx: vscode.ExtensionContext, optimizelyServ
 	ctx.subscriptions.push(
 		vscode.workspace.onDidOpenTextDocument((document) => {
 			if (document.languageId === OP_MODE_TS.language || document.languageId === OP_MODE_JS.language) {
-				updateDiagnostics(optimizelyService);
+				updateDiagnostics(optimizelyService, document);
 			}
 			return;
 		}),
 	);
-	// ctx.subscriptions.push(
-	// 	vscode.window.onDidChangeActiveTextEditor((editor) => {
-	// 		debugger;
-	// 		activeEditor = editor;
-	// 		if (editor) {
-	// 			updateDiagnostics(optimizelyService);
-	// 		}
-	// 	}),
-	// );
+	ctx.subscriptions.push(
+		vscode.window.onDidChangeActiveTextEditor((editor) => {
+			activeEditor = editor;
+			if (editor) {
+				updateDiagnostics(optimizelyService, activeEditor.document);
+			}
+		}),
+	);
 	ctx.subscriptions.push(
 		vscode.workspace.onDidChangeTextDocument((event) => {
 			if (activeEditor && event.document === activeEditor.document) {
-				updateDiagnostics(optimizelyService);
+				updateDiagnostics(optimizelyService, event.document);
 			}
 		}),
 	);
 }
 
-export function updateDiagnostics(optimizelyService: OptimizelyService): void {
-	if (optimizelyService === null || !activeEditor || !optimizelyService.getProjectId()) {
+export function updateDiagnostics(optimizelyService: OptimizelyService, document: vscode.TextDocument): void {
+	if (optimizelyService === null || !optimizelyService.getProjectId()) {
 		return;
 	}
 	diagnosticCollection.clear();
-	const regExArray = [
-		/.*\.getFeatureVariableString\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.getFeatureVariableInteger\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.getFeatureVariableBoolean\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.getFeatureVariableDouble\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.getFeatureVariableJSON\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.activate\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.isFeatureEnabled\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-		/.*\.getFeatureVariable\([\'\"][a-zA-Z0-9\_\-]+[\',\"]/g,
-	];
-	const text = activeEditor.document.getText();
+	const featureFlags = optimizelyService.getFlags();
+	const text = document.getText();
 	let match;
 	let diagnostics: vscode.Diagnostic[] = [];
-	const featureFlags = optimizelyService.getFlags();
-	for (let regEx of regExArray) {
-		while ((match = regEx.exec(text))) {
-			let featureKey = match[0].match(/'([^']+)'/)[1];
-			if (featureKey === null) {
-				// TODO: fix case with double quotes;
-				featureKey = match[0].match(/"([^"]+)"/)[1];
-			}
-			if (featureFlags.indexOf(featureKey) === -1) {
-				let startPos = activeEditor.document.positionAt(
-					match.index + match[0].lastIndexOf(featureKey),
-				);
-				let endPos = activeEditor.document.positionAt(match.index + match[0].length - 1);
-				let range = new vscode.Range(startPos, endPos);
-				diagnostics.push(
-					new vscode.Diagnostic(
-						range,
-						`Feature key ${featureKey} is not in datafile`,
-						vscode.DiagnosticSeverity.Error,
-					),
-				);
-			}
+	while (match = REGEX.exec(text)) {
+		let featureMatch = match[0].match(/'([^']+)'/);
+		if (featureMatch === null) {
+			featureMatch = match[0].match(/"([^"]+)"/);
 		}
+		let featureKey = featureMatch[1];
+		if (featureFlags.indexOf(featureKey) === -1) {
+			let startPos = document.positionAt(
+				match.index + match[0].lastIndexOf(featureKey),
+			);
+			let endPos = document.positionAt(match.index + match[0].length - 1);
+			let range = new vscode.Range(startPos, endPos);
+			diagnostics.push(
+				new vscode.Diagnostic(
+					range,
+					`Feature key ${featureKey} is not in datafile.`,
+					vscode.DiagnosticSeverity.Warning,
+				),
+			);
+		}
+
 	}
-	diagnosticCollection.set(activeEditor.document.uri, diagnostics);
+	diagnosticCollection.set(document.uri, diagnostics);
 }
